cb1b9e30484e7670c9aa7b3565bcd87483378765
Ruben <jruben.rrv95@gmail.com>
2022-02-21 14:24:07 -0600

    Busqueda sin categoria inicial

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -4,7 +4,7 @@ import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['lego']);
+    const [categories, setCategories] = useState(['']);
 
     return (
         <>
