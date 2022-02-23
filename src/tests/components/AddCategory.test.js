4705c2e25d5eb30cb924e76a7e947de4de7746b4
Ruben <jruben.rrv95@gmail.com>
2022-02-23 11:32:57 -0600

    Pruebas en addcategory

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from "enzyme"
+import { AddCategory } from "../../components/AddCategory"
+
+describe('Pruebas en el componente AddCategory', () => {
+
+    const setCategories = () => {};
+
+    test('debe mostrarse correctamente', () => {
+        const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+        expect(wrapper).toMatchSnapshot();
+    })
+})
